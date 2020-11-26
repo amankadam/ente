2153a6397502c8875eeaf48c4caa7caa0876944a
amankadam <aman.kadam.268@gmail.com>
2020-11-26 22:49:00 +0530

    improved css

--- pages/components/ImageContainer.js
+++ pages/components/ImageContainer.js
@@ -19,12 +19,15 @@ class ImageContainer extends React.Component{
         this.setState({loading:true});
       res=await axios.get('https://www.reddit.com/r/memes.json?limit=1000&after='+this.state.after);
       }
-      const modifiedList=res.data.data.children.map((v,i)=>({
+      const modifiedList=res.data.data.children.map((v,i)=>(
+        {
           src:v.data.thumbnail,
           thumbnail:v.data.thumbnail,
           title:v.data.title,
+
           width:3,height:4
-      }));
+      }
+      ));
       const newList=[...this.state.items,...modifiedList];
       this.setState({items:newList,after:res.data.data.after,loading:false});
    }
--- pages/components/Images.js
+++ pages/components/Images.js
@@ -1,25 +1,34 @@
 import React, { useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-import ModalImage from "react-modal-image";
+import styles from '../../styles/Images.module.css'; 
 import { Lightbox } from "react-modal-image";
 
 function Image(props) {
     const [open,setOpen]=useState(false);
   const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
+  const openLightbox = useCallback((event) => {
+    setCurrentImage(parseInt(event.target.attributes[2].value));
     setOpen(true);
   }, []);
-
   const onClose=()=>{
       setOpen(false);
   }
+  const imageRenderer = useCallback(
+    ({ index, key, photo }) => (
+      <img
+      className={styles.imageTile}
+      src={photo.src}
+      onClick={openLightbox}
+        key={key}
+        index={index}
+        photo={photo}
+      />
+    ),
+  );
   return (
     <div >
-      <Gallery photos={props.photos} onClick={openLightbox}  />
+      <Gallery photos={props.photos} onClick={openLightbox} renderImage={imageRenderer} />
       {
   open ?
     <Lightbox
